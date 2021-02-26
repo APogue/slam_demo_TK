2270091d533ca7f2c118cfde09a1da0d75b4e8fb
Alexie Pogue <anpogue@ucla.edu>
2021-02-26 13:42:37 -0800

    plot done for expanding window with processing time experiment

--- eval_sim_fig3.py
+++ eval_sim_fig3.py
@@ -25,10 +25,15 @@ est_opt_pt = np.zeros(num_traj)
 traj_len = range(50, 500, 50)
 
 gt_data = pd.read_csv("result/sim/exp_win_w_time/gt.csv")
-ave_error = []
 mc_ave_error = np.zeros((20, 10))
+process_time = np.zeros_like(mc_ave_error)
+ave_error = []
+process_time_array = []
+time_array = []
 for i in range(num_realizations):
 	h = 0
+	opt_time_data = pd.read_csv("result/sim/exp_win_w_time/process_time_%s.csv" % (i,))
+	process_time[i] = opt_time_data['process_time'].transpose()
 	for k in range(50, 550, 50):
 		opt_data = pd.read_csv("result/sim/exp_win_w_time/opt_%s_%s.csv" % (i, k))
 		opt_error = np.zeros_like(opt_data['p_x'])
@@ -37,44 +42,36 @@ for i in range(num_realizations):
 		mc_ave_error[i, h] = np.mean(opt_error, dtype=np.float64)
 		h += 1
 
+for k in range(50, 550, 50):
+	opt_time_data = pd.read_csv("result/sim/exp_win_w_time/opt_%s_%s.csv" % (0, k))
+	time_array.append(opt_time_data['timestamp'][len(opt_time_data)-1])
+
+
 ave_error.extend(np.mean(mc_ave_error, axis=0, dtype=np.float64))
 
-print(ave_error)
+process_time_array.extend(np.mean(process_time, axis=0, dtype=np.float64))
+print('error',ave_error)
+print('process_time',process_time_array)
+print('time', time_array)
 
 
-#
-#
-# for i in range(len(est_opt_pt)):
-# 	est_opt_time_list = []
-# 	est_em_time_list = []
-# 	est_boem_time_list = []
-# 	for k in range(0,num_realizations):
-# 		est_opt_data = pd.read_csv("result/sim/vis/opt_time_%s.csv" %k)
-# 		est_em_data = pd.read_csv("result/sim/vis/em_time_%s.csv" %k)
-# 		est_boem_data = pd.read_csv("result/sim/vis/boem_time_%s.csv" %k)
-# 		est_opt_time_list.append(est_opt_data["process_time"][i])
-# 		est_em_time_list.append(est_em_data["process_time"][i])
-# 		est_boem_time_list.append(est_boem_data["process_time"][i])
-# 	est_opt_pt[i] = (sum(est_opt_time_list)/len(est_opt_time_list))
-# 	est_em_pt[i] = (sum(est_em_time_list)/len(est_em_time_list))
-# 	est_boem_pt[i] = (sum(est_boem_time_list)/len(est_boem_time_list))
-#
-#
-#
-# fig, (ax1, ax2) = plt.subplots(2)
-# fig_width = 5.84
-# fig_height = 4.38
-# fig.set_size_inches(fig_width, fig_height)
-# line_width = 1.2
-#
-# plt.plot(traj_len, est_opt_pt, color = plot_color['opt'], linewidth=line_width, label='opt.')
-# plt.plot(traj_len, est_em_pt, color = plot_color['em'], linewidth=line_width, label='EM')
-# plt.plot(traj_len, est_boem_pt, color = plot_color['boem'], linewidth=line_width, label='BOEM')
-#
-# plt.legend()
-#
-# plt.xlabel('trajectory length [max index]')
-# plt.ylabel('time [s]')
-# # plt.ylim([0,1.31])
-#
-# plt.show()
+
+fig, (ax1, ax2) = plt.subplots(2)
+fig_width = 5.84
+fig_height = 4.38
+fig.set_size_inches(fig_width, fig_height)
+line_width = 1.2
+
+ax1.plot(time_array, ave_error, color = plot_color['opt'], linewidth=line_width, label='opt.')
+
+ax1.set(ylabel='ave error [m]')
+ax1.set_ylim(0.0, .5)
+ax1.legend(loc = 1)
+
+
+ax2.plot(time_array, process_time_array, color = plot_color['opt'], linewidth=line_width, label='opt.')
+
+ax2.set(ylabel='process time [s]')
+ax2.set(xlabel='time [s]')
+ax2.set_ylim(0.0, 20)
+plt.show()
